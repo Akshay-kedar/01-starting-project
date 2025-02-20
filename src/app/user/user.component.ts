4166d6add97f79c7e9bdf47d4f1d6cfafc83daf1
Akshay Kedar <akshay.kedar@das.cpa.com>
2025-02-20 12:39:48 +0530

    Using Angular signal to detect changes in User based on click

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from './dummy-users';
+const randomIndex=Math.floor(Math.random()*DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -9,5 +10,20 @@ import { DUMMY_USERS } from './dummy-users';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-selectedUsers=DUMMY_USERS
+  //using angular signale to check the changed value and update the data everywhere
+selectedUsers= signal(DUMMY_USERS[randomIndex])
+
+//effecient way to recompute the property if property changes
+imagepath= computed(()=>'assets/users/'+ this.selectedUsers().avatar)
+
+//get values always act as property we need not to use it as method
+// get imagepath(){
+//   return 'assets/users/'+ this.selectedUsers().avatar
+
+// }
+
+onSelectUser(){
+  const randomIndex=Math.floor(Math.random()*DUMMY_USERS.length);
+  this.selectedUsers.set(DUMMY_USERS[randomIndex])
+}
 }
