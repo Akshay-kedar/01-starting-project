2fbda98e7ddb64b4d468a97160a3fd878980819f
Akshay Kedar <akshay.kedar@das.cpa.com>
2025-02-21 09:50:36 +0530

    implemennting @input,@output and Task component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './user/dummy-users';
+import { CommonModule } from '@angular/common';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent,UserComponent],
+  imports: [HeaderComponent,TasksComponent,UserComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  users=DUMMY_USERS
+selectedUserId='u1'
+
+get SelectedUser(){
+  return this.users.find((user)=>{
+    return user.id===this.selectedUserId
+  })
+}
+
+  onSelectUser(id:string){
+   
+this.selectedUserId=id
+  }
+}
